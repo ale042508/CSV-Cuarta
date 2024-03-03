8ef983a61a93093b71934ff88bbb3d0fa45b1dff
alejandrorosa25 <109633214+alejandrorosa25@users.noreply.github.com>
2024-03-03 12:32:49 -0400

    Toda la carpeta

---
+++ Csv-master/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string[]> datos = new List<string[]>
+        {
+            new string[] {"Nombre", "Edad", "Ciudad"},
+            new string[] {"Juan", "25", "Madrid"},
+            new string[] {"María", "30", "Barcelona"},
+            new string[] {"Carlos", "28", "Valencia"}
+        };
+
+            // Ruta del archivo CSV
+            string rutaArchivo = @"C:\\Users\\Orozco\\Desktop\\mi_archivo.csv";
+
+            // Escribir en el archivo CSV
+            EscribirCSV(datos, rutaArchivo);
+
+            Console.WriteLine("Archivo CSV generado exitosamente.");
+        }
+
+        static void EscribirCSV(List<string[]> datos, string ruta)
+        {
+            using (StreamWriter writer = new StreamWriter(ruta))
+            {
+                foreach (string[] linea in datos)
+                {
+                    writer.WriteLine(string.Join(",", linea));
+                }
+            }
+        }
+    }
+}
